01cd58011dc9d4155fd4d2879e6557e0675ab95b
samikm316-boop <samikm316@gmail.com>
2026-02-25 22:01:08 +0530

    Update server.js

--- server.js
+++ server.js
@@ -191,9 +191,10 @@ app.get("/", (req, res) => {
 app.get(
   "/auth/google",
   passport.authenticate("google", {
-  scope: ["profile", "email"],
-  prompt: "select_account"
-})
+    scope: ["profile", "email"],
+    prompt: "select_account"
+  })
+);
 
 app.get(
   "/auth/google/callback",
